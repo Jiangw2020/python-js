95e134a3f60d73ac56d923743d06991bd043c5e0
Jiangwei <734145091@qq.com>
2025-09-26 23:50:22 +0800

    猿人学web 03

--- 猿人学/猿人学web-03.py
+++ 猿人学/猿人学web-03.py
@@ -1,54 +1,66 @@
-import time
-from idlelib.iomenu import encoding
 
 import requests
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+# 禁用SSL警告
+urllib3.disable_warnings(InsecureRequestWarning)
 
-import subprocess
-from functools import partial
-
-# subprocess.Popen = partial(subprocess.Popen, encoding='utf-8')
-#
-# import execjs
-#
-# with open('猿人学web-02.js', 'r+', encoding='utf-8') as f:
-#     js_code = f.read()
-#
-# ctx = execjs.compile(js_code)
+from collections import Counter
 
 
-import requests
 
-cookies = {
-    'sessionid': '5dpkiwi4jbwzy4xlpgckrfpbrmgfh3a9',
-    'no-alert3': 'true',
-    'tk': '-5942336745486126056',
-    'm': 'aa6024ad7b2a0dcf10a5053db6db006a|1758808447000',
-}
+def parse_cookie(cookie_str):
+    """解析cookie字符串为字典"""
+    parts = cookie_str.split(';')
+    cookie_dict = {}
+
+    for part in parts:
+        part = part.strip()
+        if '=' in part:
+            key, value = part.split('=', 1)
+            cookie_dict[key] = value
+        else:
+            # 处理没有值的属性（如HttpOnly）
+            cookie_dict[part] = True
+
+    return cookie_dict
+
 
 headers = {
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'priority': 'u=0, i',
-    'referer': 'https://match.yuanrenxue.cn/match/3',
+    'content-length': '0',
+    'sec-ch-ua-platform': '"Windows"',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
     'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
+    'accept': '*/*',
+    'origin': 'https://match.yuanrenxue.cn',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-    # 'cookie': 'sessionid=5dpkiwi4jbwzy4xlpgckrfpbrmgfh3a9; no-alert3=true; tk=-5942336745486126056; m=aa6024ad7b2a0dcf10a5053db6db006a|1758808447000',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://match.yuanrenxue.cn/match/3',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'priority': 'u=0, i',
+    'Cookie': 'm=85c3a103a042f2da0b003fac9b8cf29e|1758898514000; sessionid=g2hqbpd5sx2l3z9ei7fww49in2komemy; qpfccr=true; no-alert3=true; tk=-5942336745486126056',
 }
+session = requests.session()
+session.headers = headers
 
+nums = []
 for i in range(1, 6):
-    # timestamp = int(time.time() * 1000)
-    # m = ctx.call("get_m", timestamp)
-    # print(f"获取到的 m 值: {m}")
-    # cookies['m'] =  m
     params = {
         'page': str(i),
     }
-    response = requests.get('https://match.yuanrenxue.cn/api/match/3', params=params, cookies=cookies, headers=headers)
-    print(response.text)
+    jssm_response = session.post('https://match.yuanrenxue.cn/jssm', verify=False)
+    cookies = parse_cookie(jssm_response.headers['Set-Cookie'])
+
+    response = session.get('https://match.yuanrenxue.cn/api/match/3', params=params, cookies=cookies)
+    # 提取数字,并添加到列表中
+    numbers = [int(item['value']) for item in response.json()['data']]
+    nums.extend(numbers)
+
+# 统计出现次数最多的数字
+counter = Counter(nums)
+most_common = counter.most_common(1)
+print(most_common[0][0])
 
