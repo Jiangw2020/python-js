4174bee2a198ea2e8007d7c04e340e49ec4a4e5a
Jiangwei <734145091@qq.com>
2025-09-29 00:26:14 +0800

    猿人学web 07

--- 猿人学/猿人学web-06.py
+++ 猿人学/猿人学web-06.py
@@ -1,7 +1,5 @@
-import execjs
 import requests
 import time
-
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
 # 禁用SSL警告
@@ -10,8 +8,10 @@ import subprocess
 from functools import partial
 
 subprocess.Popen = partial(subprocess.Popen, encoding='utf-8')
-with open('猿人学web-06back.js', 'r+', encoding='utf-8') as f:
+with open('猿人学web-06.js', 'r+', encoding='utf-8') as f:
     js_code = f.read()
+
+import execjs
 ctx = execjs.compile(js_code)
 
 
@@ -37,14 +37,19 @@ headers = {
     'x-requested-with': 'XMLHttpRequest',
     # 'cookie': 'sessionid=h5uf8e8ctvjh2olq85t46npz7y9auurg; qpfccr=true; no-alert3=true; tk=-5942336745486126056',
 }
+total = 0
 for i in range(1, 6):
-    timetamp = int(time.time() * 1000)
-    m = ctx.call('r',timetamp,i)
+    time_tamp = int(time.time() * 1000)
+    m = ctx.call('r',time_tamp,i)
     params = {
         'page': i,
-        'q': str(i)+"-"+str(timetamp),
+        'q': str(i)+"-"+str(time_tamp)+"|",
         'm':m
     }
+
     response = requests.get('https://match.yuanrenxue.cn/api/match/6', params=params, cookies=cookies, headers=headers)
     print(response.json())
+    for item in response.json()['data']:
+        total += int(item['value'])
 
+print(total * 24)
