c25d7447ec8976ea1681d86592da32b54028f0b0
Jiangwei <734145091@qq.com>
2025-09-30 00:02:49 +0800

    猿人学web 07

--- 猿人学/猿人学web-07.py
+++ 猿人学/猿人学web-07.py
@@ -10,7 +10,7 @@ import base64
 from fontTools.ttLib import TTFont
 def init_num_map():
     map = {}
-    font = TTFont('/Users/jw/PycharmProjects/python-js/猿人学/07/0.txt')
+    font = TTFont('./07/0.txt')
     glyphNames =font.getGlyphNames()[1:]
     # https: // wakamaifondue.com / 获取数字映射数据
     nums = [1,2,4,0,7,9,8,3,5,6]
@@ -59,9 +59,11 @@ headers = {
 init_map = init_num_map()
 font_base_url = 'http://data:font/truetype;charset=utf-8;base64,'
 max_num = 0
+max_num_index = 0
+name_index = 0
 for i in range(1, 6):
     params = {
-        'page': '2',
+        'page': i,
     }
     response = requests.get('https://match.yuanrenxue.cn/api/match/7', params=params, cookies=cookies, headers=headers)
     woff = response.json()['woff']
@@ -76,12 +78,17 @@ for i in range(1, 6):
         f.write(base64.b64decode(woff))
 
     new_map = get_new_num_map(init_map, file_path)
-
+    index = 1
     for item in response.json()['data']:
+        name_index = 10* (i-1) + index
+        index += 1
         current_num = ''
         for uni in item['value'].split():
-
             current_num = current_num + str(new_map[uni.replace('&#x','')])
         max_num = max(max_num, int(current_num))
-
+        if max_num == int(current_num):
+            max_num_index = name_index
+names = ['极镀ギ紬荕', '爷灬霸气傀儡', '梦战苍穹', '傲世哥', 'мaη肆風聲', '一刀メ隔世', '横刀メ绝杀', 'Q不死你R死你', '魔帝殤邪', '封刀不再战', '倾城孤狼', '戎马江湖', '狂得像风', '影之哀伤', '謸氕づ独尊', '傲视狂杀', '追风之梦', '枭雄在世', '傲视之巅', '黑夜刺客', '占你心为王', '爷来取你狗命', '御风踏血', '凫矢暮城', '孤影メ残刀', '野区霸王', '噬血啸月', '风逝无迹', '帅的睡不着', '血色杀戮者', '冷视天下', '帅出新高度', '風狆瑬蒗', '灵魂禁锢', 'ヤ地狱篮枫ゞ', '溅血メ破天', '剑尊メ杀戮', '塞外う飛龍', '哥‘K纯帅', '逆風祈雨', '恣意踏江山', '望断、天涯路', '地獄惡灵', '疯狂メ孽杀', '寂月灭影', '骚年霸称帝王', '狂杀メ无赦', '死灵的哀伤', '撩妹界扛把子', '霸刀☆藐视天下', '潇洒又能打', '狂卩龙灬巅丷峰', '羁旅天涯.', '南宫沐风', '风恋绝尘', '剑下孤魂', '一蓑烟雨', '领域★倾战', '威龙丶断魂神狙', '辉煌战绩', '屎来运赚', '伱、Bu够档次', '九音引魂箫', '骨子里的傲气', '霸海断长空', '没枪也很狂', '死魂★之灵'];
 print(max_num)
+print(max_num_index)
+print(names[max_num_index])
